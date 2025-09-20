a57886c799714d8cc83f96219305e29be7e60e1e
Vishal <vishalbanthotra@gmail.com>
2025-09-20 08:49:16 +0530

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+const button=document.getElementById("btn");
+const nav=document.querySelector(".nav");
+const menu=document.querySelector(".a");
+let name=document.querySelector(".name").value;
+let email=document.querySelector(".email").value;
+let select=document.querySelector("#select").value;
+let btn=document.querySelector(".submit")
+button.addEventListener("click",()=>{
+  nav.classList.toggle("show");
+})
